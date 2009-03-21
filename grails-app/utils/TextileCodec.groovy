8aa0c3f1f2fe0d003724e8f49bdb13e8ba3bd161
mstine <matt@mattstine.com>
2009-03-21 00:13:42 -0500

    switch out textile library

--- grails-app/utils/TextileCodec.groovy
+++ grails-app/utils/TextileCodec.groovy
@@ -1,18 +1,9 @@
-import org.eclipse.mylyn.wikitext.core.parser.MarkupParser
-import org.eclipse.mylyn.wikitext.textile.core.TextileLanguage
-import org.eclipse.mylyn.wikitext.core.parser.builder.HtmlDocumentBuilder
+import com.plink.plextile.TextParser
 
 class TextileCodec {
 
   static encode = {str ->
-    StringWriter out = new StringWriter()
-
-    HtmlDocumentBuilder documentBuilder = new HtmlDocumentBuilder(out)
-    documentBuilder.setEmitAsDocument(false)
-
-    new MarkupParser(new TextileLanguage(), documentBuilder).parse(str)
-    return out.toString()
-
+    new TextParser().parseTextile(str, true)
   }
 
 }
