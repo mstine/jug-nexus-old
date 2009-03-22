b721a33ce2452bc98691d96d7b0d509e514b5112
mstine <matt@mattstine.com>
2009-03-22 15:04:08 -0500

    security configuration

---
+++ grails-app/conf/SecurityConfig.groovy
@@ -0,0 +1,12 @@
+security {
+
+	// see DefaultSecurityConfig.groovy for all settable/overridable properties
+
+	active = true
+
+	loginUserDomainClass = "Person"
+	authorityDomainClass = "Authority"
+
+    useRequestMapDomainClass = false
+    useControllerAnnotations = true
+}
