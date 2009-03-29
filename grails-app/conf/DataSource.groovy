8dbcc4e28dc54d9cb38f5660768f4ff4fe9c0834
mstine <matt@mattstine.com>
2009-03-29 04:25:06 -0500

    move prod to jndi datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,36 +1,35 @@
-dataSource {
-	pooled = true
-	driverClassName = "com.mysql.jdbc.Driver"
-}
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
-    cache.provider_class='com.opensymphony.oscache.hibernate.OSCacheProvider'
+  cache.use_second_level_cache = true
+  cache.use_query_cache = true
+  cache.provider_class = 'com.opensymphony.oscache.hibernate.OSCacheProvider'
 }
 // environment specific settings
 environments {
-	development {
-		dataSource {
-            username = "root"
-            password = ""
-			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-			url = "jdbc:mysql://localhost/jugnexusdev"
-		}
-	}
-	test {
-		dataSource {
-            username = "root"
-            password = ""
-			dbCreate = "update"
-			url = "jdbc:mysql://localhost/jugnexustest"
-		}
-	}
-	production {
-		dataSource {
-			username = "root"
-            password = ""
-			dbCreate = "update"
-			url = "jdbc:mysql://localhost/jugnexusprod"
-		}
-	}
+  development {
+    dataSource {
+      pooled = true
+      username = "root"
+      password = ""
+      dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+      driverClassName = "com.mysql.jdbc.Driver"
+      url = "jdbc:mysql://localhost/jugnexusdev"
+    }
+  }
+  test {
+    dataSource {
+      pooled = true
+      username = "root"
+      password = ""
+      dbCreate = "update"
+      driverClassName = "com.mysql.jdbc.Driver"
+      url = "jdbc:mysql://localhost/jugnexustest"
+    }
+  }
+  production {
+    dataSource {
+      pooled = false
+      dbCreate = "update"
+      jndiName = "java:comp/env/jdbc/JugNexusProdDB"
+    }
+  }
 }
